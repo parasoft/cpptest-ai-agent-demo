a67f5740b2d5d06ec97aa96a3336fc4dfa9c9d7d
Piotr Krukowiecki <34373523+pkrukp@users.noreply.github.com>
2026-05-19 10:19:08 +0200

    Do not fail if copilot mcp server is already registered.
    Only add "cpptest-std-mcp" mcp server if it's not already registered.

--- .github/workflows/cpptest-agent-run.sh
+++ .github/workflows/cpptest-agent-run.sh
@@ -14,7 +14,7 @@ codex exec -s danger-full-access --config allow_login_shell=false "$(cat "$(dirn
 # == Copilot ==
 
 # Register C/C++test MCP server - assuming C/C++test is installed in '/opt/parasoft/cpptest' location, adjust as needed
-# copilot mcp add cpptest-std-mcp -- /opt/parasoft/cpptest/integration/mcp/cpptestmcp
+# copilot mcp get cpptest-std-mcp >/dev/null 2>&1 || copilot mcp add cpptest-std-mcp -- /opt/parasoft/cpptest/integration/mcp/cpptestmcp
 
 # Execute the prompt with Copilot - be sure to adjust sandbox permissions as needed for your prompt
 # copilot --allow-all --no-ask-user -s -p "$(cat "$(dirname "$0")/cpptest-agent-prompt.md")"
